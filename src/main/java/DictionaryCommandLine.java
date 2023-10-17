9cff9b64a67d34d1f9f530224bc1a5bd6bfb3444
ngquan244 <125577135+ngquan244@users.noreply.github.com>
2023-10-17 13:18:35 +0700

    Add file reader.

--- src/main/java/DictionaryCommandLine.java
+++ src/main/java/DictionaryCommandLine.java
@@ -1,4 +1,8 @@
 import org.oopvnu.DictionaryCommandline;
+import org.oopvnu.management.DictionaryManagement;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class DictionaryCommandLine {
     DictionaryCommandline dictionaryCommandline;
@@ -10,9 +14,21 @@ public class DictionaryCommandLine {
         dictionaryCommandline.getDictionaryManagement().insertFromCommandline();
         dictionaryCommandline.showAllWords();
     }
+    public void readFromFileTest(String filename) throws IOException, FileNotFoundException {
+        try {
+            dictionaryCommandline.getDictionaryManagement().insertFromFile(filename);
+            dictionaryCommandline.showAllWords();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String filename = "src\\main\\java\\org\\oopvnu\\management/dictionaries.txt";
+
         DictionaryCommandLine dictionaryCommandLine = new DictionaryCommandLine();
-        dictionaryCommandLine.dictionaryBasic();
+
+//        dictionaryCommandLine.dictionaryBasic();
+        dictionaryCommandLine.readFromFileTest(filename);
     }
 }
