eaa9f34947abce5a19066b0a8c3359d510e397b8
Tran Dinh Phu <56244176+cdf144@users.noreply.github.com>
2023-11-07 14:00:17 +0700

    Add DictionaryCommandLine log file

--- src/main/java/org/nora/dictionary/DictionaryCommandLine.java
+++ src/main/java/org/nora/dictionary/DictionaryCommandLine.java
@@ -3,12 +3,30 @@ package org.nora.dictionary;
 import org.nora.dictionary.management.DictionaryCommandline;
 import org.nora.dictionary.management.DictionaryManagement;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DictionaryCommandLine {
     private static final Logger LOGGER = Logger.getLogger(DictionaryManagement.class.getName());
+
+    public static final String PATH_DICTIONARYMANAGEMENT_LOG = System.getProperty("user.dir")
+            + File.separator + "log"
+            + File.separator + "logDictionaryCommandLine.log";
+
+    private static final FileHandler logFileHandler;
+    static {
+        try {
+            logFileHandler = new FileHandler(PATH_DICTIONARYMANAGEMENT_LOG, false);
+            logFileHandler.setLevel(Level.INFO);
+            LOGGER.addHandler(logFileHandler);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initalize DictionaryCommandLine log FileHandler!", e);
+        }
+    }
+
     DictionaryCommandline dictionaryCommandline;
 
     public DictionaryCommandLine() {
