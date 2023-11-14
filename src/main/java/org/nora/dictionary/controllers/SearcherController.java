1af7c4a14bec9b7148f088ecb729f5c0becaf8eb
Tran Dinh Phu <56244176+cdf144@users.noreply.github.com>
2023-11-14 08:43:10 +0700

    Basic Dictionary searching functions done

--- src/main/java/org/nora/dictionary/controllers/SearcherController.java
+++ src/main/java/org/nora/dictionary/controllers/SearcherController.java
@@ -3,6 +3,7 @@ package org.nora.dictionary.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -10,17 +11,25 @@ import javafx.scene.web.WebView;
 import org.nora.dictionary.DictionaryApplication;
 import org.nora.dictionary.entities.Word;
 
-public class SearcherController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearcherController implements Initializable {
     @FXML
     private TextField searchField;
     @FXML
     private ListView<String> autocompleteList;
     @FXML
-    private Label wordTarget;
+    private Label wordTargetLabel;
     @FXML
-    private WebView wordExplain;
+    private WebView wordExplainView;
 
     private final ObservableList<String> autocompleteWordList = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        updateAutocompleteList();
+    }
 
     public void updateAutocompleteList() {
         autocompleteWordList.clear();
@@ -40,7 +49,18 @@ public class SearcherController {
         autocompleteList.setItems(autocompleteWordList);
     }
 
-    public void searchFieldOnKeyReleased() {
+    public void searchFieldOnKeyTyped() {
         updateAutocompleteList();
+    }
+
+    public void autocompleteListOnClick() {
+        String target = autocompleteList.getSelectionModel().getSelectedItem();
+        int index =
+                DictionaryApplication.dictionary.getDictionary().findWord(
+                        new Word(target.toLowerCase(), null)
+                );
+        Word word = DictionaryApplication.dictionary.getDictionary().getWordList().get(index);
+        wordTargetLabel.setText(word.getTarget());
+        wordExplainView.getEngine().loadContent(word.getExplain(), "text/html");
     }
 }
