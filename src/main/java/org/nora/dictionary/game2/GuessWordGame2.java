846f9d51c8fa8f5cc4a2b39ce3127a9a832933aa
ngquan244 <125577135+ngquan244@users.noreply.github.com>
2023-11-19 00:23:15 +0700

    GuessWordGame2 là t dựa trên GuessWordGame nhưng khác là bổ sung file ảnh vs tệp các từ t tự soạn, hiện vẫn chưa lên frontend nên t làm tạm đưa ra đường dẫn thôi

---
+++ src/main/java/org/nora/dictionary/game2/GuessWordGame2.java
@@ -0,0 +1,94 @@
+package org.nora.dictionary.game2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessWordGame2 {
+
+    private List<String> wordList;
+    private List<String> imageList;
+    private int score;
+
+    public GuessWordGame2() {
+        this.wordList = generateWordList("src\\main\\resources\\GuessGame.txt");
+        this.imageList = generateImageList("src\\main\\resources\\GuessGameImage", this.wordList);
+        this.score = 0;
+    }
+
+    private List<String> generateWordList(String filePath) {
+        List<String> words = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                words.add(line.trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return words;
+    }
+
+    private List<String> generateImageList(String folderPath, List<String> wordList) {
+        List<String> images = new ArrayList<>();
+
+        for (String word : wordList) {
+            String imagePath = folderPath + "/" + word + ".jpg";
+            images.add(imagePath);
+        }
+
+        return images;
+    }
+
+    public void startGame() {
+        List<String> options = List.of("A", "B", "C", "D");
+        Random random = new Random();
+
+        for (int i = 0; i < wordList.size(); i++) {
+            System.out.println("Câu hỏi: Đây là hình ảnh của từ gì?");
+            System.out.println("Ảnh: " + imageList.get(i));
+
+            int correctIndex = random.nextInt(4);
+            int optionIndex = 0;
+
+            for (int j = 0; j < 4; j++) {
+                if (j == correctIndex) {
+                    System.out.println(options.get(optionIndex) + ". " + wordList.get(i));
+                } else {
+                    int randomWordIndex;
+                    do {
+                        randomWordIndex = random.nextInt(wordList.size());
+                    } while (randomWordIndex == i);
+
+                    System.out.println(options.get(optionIndex) + ". " + wordList.get(randomWordIndex));
+                }
+                optionIndex++;
+            }
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Nhập câu trả lời (A/B/C/D): ");
+            String userAnswer = scanner.nextLine().toUpperCase();
+
+            if (userAnswer.equals(options.get(correctIndex))) {
+                score += 5;
+                System.out.println("Chính xác! Điểm của bạn: " + score);
+            } else {
+                score -= 10;
+                System.out.println("Sai rồi! Điểm của bạn: " + score);
+            }
+        }
+
+        System.out.println("Trò chơi kết thúc. Điểm số cuối cùng của bạn: " + score);
+    }
+
+    public static void main(String[] args) {
+        GuessWordGame2 game = new GuessWordGame2();
+        game.startGame();
+    }
+}
