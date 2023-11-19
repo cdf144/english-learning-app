631dc3615d849423a2e2ade148b0d06ebc034226
ngquan244 <125577135+ngquan244@users.noreply.github.com>
2023-11-20 00:55:18 +0700

    chỉnh thêm xíu

--- src/main/java/org/nora/dictionary/controllers/GameController.java
+++ src/main/java/org/nora/dictionary/controllers/GameController.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 
 public class GameController {
     @FXML
-    public void onStartGameButtonClick() throws IOException {
+    public void onStartGuessGameButtonClick() throws IOException {
         Parent root;
         root = FXMLLoader.load(
                 Objects.requireNonNull(
@@ -20,7 +20,21 @@ public class GameController {
                 )
         );
         Stage stage = new Stage();
-        stage.setTitle("Game");
+        stage.setTitle("Guess Word Game");
+        stage.setScene(new Scene(root, 1100, 705));
+        stage.show();
+    }
+
+    @FXML
+    public void onStartShuffleGameButtonClick() throws IOException {
+        Parent root;
+        root = FXMLLoader.load(
+                Objects.requireNonNull(
+                        DictionaryApplication.class.getResource("shufflegame.fxml")
+                )
+        );
+        Stage stage = new Stage();
+        stage.setTitle("Shuffle Game");
         stage.setScene(new Scene(root, 1100, 705));
         stage.show();
     }
